de85bcba1f6b6fdc7677dd0ee7b19c00dbc9ec3f
m.jacobsem <mauriciojacobsen55@gmail.com>
2024-07-05 00:18:22 -0300

    Atualizado todos os códigos

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
-const hours = document.getElementById("hours")
-const minutes = document.getElementById("minutes")
-const seconds = document.getElementById("seconds")
+// RELOGIO DIGITAL
+function data() {
+    let dataAtual = new Date();
+    const day = dataAtual.getDate();
+    const month = dataAtual.getMonth() + 1;
+    const year = dataAtual.getFullYear();
+    const dayFormatted = `${day}/${month}/${year}`
+    document.getElementById('date').innerText = dayFormatted;
+}
+data()
 
-setInterval(() => {
-    const actualDate = new Date();
-    
-    hours.innerHTML = actualDate.getHours()
-    minutes.innerHTML = actualDate.getMinutes()
-    seconds.innerHTML = actualDate.getSeconds()
-},1000)
+function horario() {
+    setInterval(() => {
+            let dataAtual = new Date();
+            const hours = dataAtual.getHours().toString().padStart(2, '0');
+            const minutes = dataAtual.getMinutes().toString().padStart(2, '0');
+            const seconds = dataAtual.getSeconds().toString().padStart(2, '0');
+            const formattedTime = `${hours} : ${minutes} : ${seconds}`
+            document.getElementById('time').innerText = formattedTime
+    }, 1000);
+}
+horario()
+
+// TIMER
+document.getElementById('time-button').addEventListener('click', function(){
+    document.getElementById('container-clock').style.display = 'block';
+    document.getElementById('container-timer').style.display = 'none';
+})
+document.getElementById('timer-button').addEventListener('click', function(){
+    document.getElementById('container-clock').style.display = 'none';
+    document.getElementById('container-timer').style.display = 'block';
+})
+
+let timerInterval;
+let timerSeconds = 0;
+
+document.getElementById('start-timer').addEventListener('click', function(){
+    if (!timerInterval) {
+        timerInterval = setInterval(() => {
+            timerSeconds++
+            const hours = String(Math.floor(timerSeconds / 3600)).padStart(2, '0');
+            const minutes = String(Math.floor(timerSeconds % 3600 / 60)).padStart(2, '0');
+            const seconds = String(timerSeconds % 60).padStart(2, '0');
+            document.getElementById('timer').innerText = `${hours} : ${minutes} : ${seconds}`
+        }, 1000)
+    }
+})
+
+document.getElementById('stop-timer').addEventListener('click', function(){
+    clearInterval(timerInterval)
+    timerInterval = null;
+});
+
+document.getElementById('zerar-timer').addEventListener('click', function() {
+    clearInterval(timerInterval);
+    timerInterval = null;
+    timerSeconds = 0;
+    document.getElementById('timer').innerText = '00 : 00 : 00';
+});
